884deebaac5930e62c96f3a3c16e38494edee331
ikemura <8417910+banbara23@users.noreply.github.com>
2018-04-09 23:14:20 +0900

    add tab activity/fragment

---
+++ app/src/main/java/ikemura/com/yaeyama_hash_tag_android/TabActivityFragment.kt
@@ -0,0 +1,18 @@
+package ikemura.com.yaeyama_hash_tag_android
+
+import android.support.v4.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+class TabActivityFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_tab, container, false)
+    }
+}
