b6695b28154b23d927ff9bb7e9c359ca50b81da8
ikemura <8417910+banbara23@users.noreply.github.com>
2018-04-10 23:59:38 +0900

    add databinding

--- app/src/main/java/ikemura/com/yaeyama_hash_tag_android/MainFragment.kt
+++ app/src/main/java/ikemura/com/yaeyama_hash_tag_android/MainFragment.kt
@@ -1,17 +1,26 @@
 package ikemura.com.yaeyama_hash_tag_android
 
+import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import ikemura.com.yaeyama_hash_tag_android.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
+    lateinit var binding: MainFragmentBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false)
+        binding.fragment = this
+        return binding.root
+    }
+
+     fun navigateToTabActivity(s: String) {
+        startActivity(Intent(activity, TabActivity::class.java))
     }
 
 }
