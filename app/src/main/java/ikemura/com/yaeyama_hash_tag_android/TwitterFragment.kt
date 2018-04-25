4b920e36b3d64aa8691290f256013b06a9a39848
ikemura <8417910+banbara23@users.noreply.github.com>
2018-04-26 08:09:54 +0900

    タブ文字を小文字を表示可能にした

--- app/src/main/java/ikemura/com/yaeyama_hash_tag_android/TwitterFragment.kt
+++ app/src/main/java/ikemura/com/yaeyama_hash_tag_android/TwitterFragment.kt
@@ -1,12 +1,14 @@
 package ikemura.com.yaeyama_hash_tag_android
 
 
+import android.annotation.SuppressLint
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import ikemura.com.yaeyama_hash_tag_android.databinding.FragmentTwitterBinding
 
 
@@ -36,10 +38,14 @@ class TwitterFragment : Fragment() {
         }
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_twitter, container, false)
+        binding.webView.webViewClient = WebViewClient()
+        binding.webView.loadUrl("https://twitter.com/search?q=%23%E5%85%AB%E9%87%8D%E5%B1%B1&src=typd&lang=ja")
+        binding.webView.settings.javaScriptEnabled = true
         return binding.root
     }
 
