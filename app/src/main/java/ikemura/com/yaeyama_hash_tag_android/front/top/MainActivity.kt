1b21687dc948ecd08f254a0e176061e765c2a1cd
ikemura <k_ikemura@yumemi.co.jp>
2018-05-09 13:27:47 +0900

    add new relic sdk

--- app/src/main/java/ikemura/com/yaeyama_hash_tag_android/front/top/MainActivity.kt
+++ app/src/main/java/ikemura/com/yaeyama_hash_tag_android/front/top/MainActivity.kt
@@ -2,6 +2,7 @@ package ikemura.com.yaeyama_hash_tag_android.front.top
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.newrelic.agent.android.NewRelic
 import ikemura.com.yaeyama_hash_tag_android.R
 
 /**
@@ -11,6 +12,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // initialize New Relic
+        NewRelic.withApplicationToken(
+                "AA9ef2258058501fe6deb0ce5a36b392c63b6e09ab"
+        ).start(this.getApplication())
+
+        // initialize Fragment
         setContentView(R.layout.main_activity)
         supportFragmentManager
                 .beginTransaction()
